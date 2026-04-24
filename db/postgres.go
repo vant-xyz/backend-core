9db63134d9342cc939282deadeb103d0fe90156a
DavidNzube101 <davidnzube2007@outlook.com>
2026-04-24 13:29:10 +0100

    fix: use simple query protocol to prevent prepared statement conflicts on restart

--- db/postgres.go
+++ db/postgres.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -22,6 +23,7 @@ func Init(databaseURL string) {
 	cfg.MaxConnLifetime = 30 * time.Minute
 	cfg.MaxConnIdleTime = 5 * time.Minute
 	cfg.HealthCheckPeriod = 1 * time.Minute
+	cfg.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
 
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
